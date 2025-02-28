3ce9ac0bc14401529c3717f03834e282d8f79974
iamtheluiz <iamtheluiz.dev@gmail.com>
2025-02-28 12:45:58 -0300

    feat: add full screen support to slide component with dynamic styling

--- src/app/components/slide/slide.component.ts
+++ src/app/components/slide/slide.component.ts
@@ -1,11 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-slide',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './slide.component.html',
   styleUrl: './slide.component.scss'
 })
 export class SlideComponent {
   @Input() html = "";
+  @Input() full = false;
 }
