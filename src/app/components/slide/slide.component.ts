86b5e359ab1f97579eaffa1758d02334e1a9c973
iamtheluiz <iamtheluiz.dev@gmail.com>
2025-03-23 13:40:05 -0300

    feat: implement responsive font size and padding in slide component

--- src/app/components/slide/slide.component.ts
+++ src/app/components/slide/slide.component.ts
@@ -1,13 +1,43 @@
-import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, DoCheck, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-slide',
-  imports: [NgClass],
+  imports: [],
   templateUrl: './slide.component.html',
   styleUrl: './slide.component.scss'
 })
-export class SlideComponent {
+export class SlideComponent implements AfterViewInit, DoCheck {
   @Input() html = "";
   @Input() full = false;
+
+  @ViewChild('slide') slide: any;
+
+  relativeFontSize = 0;
+  relativePadding = 0;
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit(): void {
+    this.calculateRelativeSize();
+  }
+
+  ngDoCheck(): void {
+    this.calculateRelativeSize();
+  }
+
+  calculateRelativeSize() {
+    console.log("Oi!");
+    if (!this.slide) return;
+
+    const baseWidth = 1920;
+    const baseFontSize = 42;
+    const basePadding = 64;
+
+    const slideElement = this.slide.nativeElement as HTMLDivElement;
+
+    this.relativeFontSize = Number((slideElement.clientWidth * baseFontSize) / baseWidth);
+    this.relativePadding = Number((slideElement.clientWidth * basePadding) / baseWidth);
+
+    this.cdr.detectChanges();
+  }
 }
